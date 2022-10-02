01ea87e6b7b374e2e95e4b7c72f7bb051b683d2b
Mirel <valerymirel@mail.ru>
2022-10-03 00:20:19 +0500

    bubble

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -5,11 +5,19 @@ using namespace std;
 void bubbleSort(int arr[], int n)
 {
     int i, j;
-    bool flag;
+    bool flag = false;
     for (i = 0; i < n - 1; i++)
+    {
+        if (flag)
+            break;
+        flag = true;
         for (j = 0; j < n - i - 1; j++)
             if (arr[j] > arr[j + 1])
+            {
                 swap(arr[j], arr[j + 1]);
+                flag = false;
+            }
+    }
 }
 
 int main()
